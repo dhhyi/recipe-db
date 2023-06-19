383b5196c8bccf3be8d44d3eeb8560089c409aac
Danilo Hoffmann <dhhyi@aol.com>
2023-06-19 10:06:45 +0200

    install deps in synchronize

--- synchronize.js
+++ synchronize.js
@@ -1,7 +1,25 @@
 const fs = require("fs");
 const cp = require("child_process");
-const yaml = require("js-yaml");
 const path = require("path");
+
+if (!process.env._?.endsWith("/git")) {
+  cp.execSync("npm exec pnpm install", { cwd: __dirname, stdio: "inherit" });
+
+  fs.readdirSync(__dirname).forEach((file) => {
+    if (
+      fs.statSync(path.join(__dirname, file)).isDirectory() &&
+      fs.existsSync(path.join(__dirname, file, "pnpm-lock.yaml"))
+    ) {
+      console.log(`Installing dependencies in ${file}...`);
+      cp.execSync("npm exec pnpm install", {
+        cwd: path.join(__dirname, file),
+        stdio: "inherit",
+      });
+    }
+  });
+}
+
+const yaml = require("js-yaml");
 
 function flattenObject(obj) {
   const result = [];
