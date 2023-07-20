86ffd5051260f3ac41b9193dca5547e11257b818
Danilo Hoffmann <dhhyi@aol.com>
2023-07-20 09:00:39 +0200

    use existing containers as much as possible

--- .scripts/run-in-devcontainer.js
+++ .scripts/run-in-devcontainer.js
@@ -1,6 +1,7 @@
 const cp = require("child_process");
 const fs = require("fs");
-const { languageFile, getProjectConfig } = require("./shared");
+const path = require("path");
+const { languageFile, getProjectConfig, projectRoot } = require("./shared");
 
 const project = process.argv[2];
 const command = process.argv.slice(3);
@@ -44,26 +45,93 @@ if (command.length === 1 && command[0] === "test") {
 }
 
 let containerId = null;
+let stopAfter = true;
 
 try {
-  const commandLine = `devcontainer up --workspace-folder ${project}`;
-  // if (process.env.PRE_COMMIT) {
-  //   commandLine += " --remove-existing-container";
-  // }
-  const output = cp.execSync(commandLine, {
-    stdio: ["ignore", "pipe", "inherit"],
-    encoding: "utf-8",
-  });
-  const json = JSON.parse(output);
-  if (json?.outcome !== "success") {
-    throw new Error("Failed to start devcontainer");
+  // search for existing container
+  const workspaceDevcontainerJson = path.join(
+    projectRoot,
+    project,
+    ".devcontainer",
+    "devcontainer.json"
+  );
+  const existingRunningContainerID = cp
+    .execSync(
+      `docker ps --filter "label=devcontainer.config_file=${workspaceDevcontainerJson}" --filter "status=running" --format '{{.ID}}'`,
+      { encoding: "utf-8" }
+    )
+    ?.trim();
+  if (existingRunningContainerID) {
+    console.log(
+      `Using existing running container ${existingRunningContainerID}`
+    );
+    containerId = existingRunningContainerID;
+    stopAfter = false;
+  } else {
+    const existingStoppedContainerID = cp
+      .execSync(
+        `docker ps -a --filter "label=devcontainer.config_file=${workspaceDevcontainerJson}" --filter "exited=0" --format '{{.ID}}'`,
+        { encoding: "utf-8" }
+      )
+      ?.trim();
+    if (
+      existingStoppedContainerID &&
+      existingStoppedContainerID.split("\n").length === 1
+    ) {
+      console.log(
+        `Starting existing stopped container ${existingStoppedContainerID}`
+      );
+      cp.execSync(`docker start ${existingStoppedContainerID}`);
+      containerId = existingStoppedContainerID;
+    } else {
+      const existingContainerIDs = cp
+        .execSync(
+          `docker ps -a --filter "label=devcontainer.config_file=${workspaceDevcontainerJson}" --format '{{.ID}}'`,
+          { encoding: "utf-8" }
+        )
+        ?.trim();
+      existingContainerIDs
+        .split("\n")
+        .filter(Boolean)
+        .forEach((id) => {
+          console.log(`Removing existing container ${id}`);
+          cp.execSync(`docker rm -f ${id}`);
+        });
+    }
   }
 
-  containerId = json?.containerId;
+  // start container if not found
+  if (!containerId) {
+    const commandLine = `devcontainer up --workspace-folder ${project}`;
+    const output = cp.execSync(commandLine, {
+      stdio: ["ignore", "pipe", "inherit"],
+      encoding: "utf-8",
+    });
+    const json = JSON.parse(output);
+    if (json?.outcome !== "success") {
+      throw new Error("Failed to start devcontainer");
+    }
+    containerId = json?.containerId;
+  }
 
+  // get workspace mount of container
+  const workspaceMounts = JSON.parse(
+    cp.execSync(
+      `docker inspect --type container ${containerId} --format '{{json .Mounts}}'`
+    )
+  );
+  const workspaceMount = workspaceMounts.find(
+    (m) => m.Type === "bind" && m.Source.startsWith(projectRoot)
+  );
+  if (!workspaceMount) {
+    throw new Error("Failed to find workspace mount");
+  }
+  const workspaceFolder = path.join(workspaceMount.Destination, project);
+
+  // run command in container
   const result = cp.spawnSync(
-    "devcontainer",
-    ["exec", "--workspace-folder", project, ...runCommand],
+    "docker",
+    ["exec", "--workdir", workspaceFolder, containerId, ...runCommand],
     { stdio: "inherit", encoding: "utf-8" }
   );
 
@@ -72,7 +140,10 @@ try {
     process.exit(result.status);
   }
 } finally {
-  if (containerId) {
+  if (containerId && stopAfter) {
+    console.log(`Stopping container ${containerId}`);
     cp.execSync(`docker stop ${containerId}`);
+  } else {
+    console.log(`Leaving container ${containerId} running`);
   }
 }
