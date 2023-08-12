a8c7962764e8ffa1498bd493eeda6123a38152b7
Danilo Hoffmann <dhhyi@aol.com>
2023-08-12 11:33:01 +0200

    use new listen method

--- frontend/server.mjs
+++ frontend/server.mjs
@@ -44,9 +44,15 @@ server.get("/health", async (_, res) => {
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "0.0.0.0";
 
-server.listen(port, host, (err) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-});
+server.listen(
+  {
+    port,
+    host,
+  },
+  (err) => {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  },
+);
