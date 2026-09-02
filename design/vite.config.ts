5ec668bdef65c6feadfbae9a29332fded6814c5c
Danilo Hoffmann <dhhyi@aol.com>
2026-09-02 11:43:31 +0200

    serve design via nginx

--- design/vite.config.ts
+++ design/vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
+    rolldownOptions: {
+      output: {
+        assetFileNames: "design/[name]-[hash].[ext]",
+        chunkFileNames: "design/[name]-[hash].js",
+        entryFileNames: "design/[name]-[hash].js",
+      },
+    },
     minify: true,
     cssMinify: true,
   },
